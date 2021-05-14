624a4ea4a43f39e2a2dd898f689bea1491efa006
Jonathan Mejia <jonathanmejia.ar@gmail.com>
2021-05-14 15:48:27 -0300

    refactor: new folder utils and comments added

--- server/src/index.js
+++ server/src/index.js
@@ -1,44 +1,57 @@
 const express = require('express');
 const http = require('http');
+const moment = require('moment');
+const socketio = require('socket.io');
+const { joinRoom, leaveRoom, getRoomUsers, getCurrentUser } = require('./utils/users')
 const app = express();
 const server = http.createServer(app);
-const moment = require('moment');
 
-const socketio = require('socket.io');
 const io = socketio(server, {
     cors: {
         origin: "*",
     }
 });
-const { joinRoom, leaveRoom, getRoomUsers } = require('./users')
 
+//Run when client connects
 io.on('connection', (socket) => {
 
-    let currentUser;
     socket.on('connected', (name, room) => {
-        currentUser = joinRoom(socket.id, name, room);
+        //User joins the room
+        const currentUser = joinRoom(socket.id, name, room);
         socket.join(currentUser.room);
+
+        //Send a connection notification and room info
         const time = moment().format('h:mm:ss');
         socket.to(currentUser.room).emit('messages', { name: currentUser.name, message: `Has joined the room`, time });
         const roomUsers = getRoomUsers(currentUser.room);
         io.to(currentUser.room).emit('users-online', roomUsers);
     });
 
+    //Listen for message and emit the message
     socket.on('message', (name, message) => {
+        const currentUser = getCurrentUser(socket.id);
         const time = moment().format('h:mm:ss');
         io.to(currentUser.room).emit('messages', { name, message, time });
     });
 
+    //Broadcast when a user is typing
     socket.on('typing', (name) => {
+        const currentUser = getCurrentUser(socket.id);
         socket.to(currentUser.room).emit('user-typing', name);
     });
 
+    //Run when client disconnects
     socket.on('disconnect', () => {
+        const currentUser = getCurrentUser(socket.id);
+
         if (currentUser) {
             leaveRoom(currentUser.id);
+            //Send actual room info
             const roomUsers = getRoomUsers(currentUser.room);
             io.to(currentUser.room).emit('users-online', roomUsers);
             socket.to(currentUser.room).emit('user-typing', '');
+
+            //Send a disconnect notification
             const time = moment().format('h:mm:ss');
             io.to(currentUser.room).emit('messages', { name: currentUser.name, message: `Has left the room`, time });
         }
